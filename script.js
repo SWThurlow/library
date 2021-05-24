2b91b8bb261cf4d03d383463356f9cba1360f808
Samuel Thurlow <thurlow.webstuff@gmail.com>
2021-05-24 09:33:52 +0100

    ADDED FUNCTIONS FOR BOOK REMOVAL AND TOGGLING READ STATUS
    Added a function to toggle the read status of a book and another
    to remove a book. An eventlistener was added to the library element
    to listen out for any clicks on the buttons to call either function.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /*Array for books and retrieving html elements.*/
 let myLibrary = [];
 
-const additionTitle = document.getElementById('title');
-const additionAuthoor = document.getElementById('author');
-const additionPageCount = document.getElementById('pageCount');
-const additionRead = document.getElementById('read');
-const addAddition = document.getElementById('addBook')
+const newTitle = document.getElementById('title');
+const newAuthor = document.getElementById('author');
+const newPageCount = document.getElementById('pageCount');
+const newRead = document.getElementById('read');
+const addBook = document.getElementById('addBook')
 const library = document.querySelector('.library');
 
 /*Book constructor.*/
@@ -15,55 +15,81 @@ class Book {
         this.author = author;
         this.pageCount = pageCount;
         this.read = read;
+        this.index;
+        this.displayed = false;
+    }
+    findIndex(){
         this.index = myLibrary.indexOf(this);
     }
 }
 
 /*Adding new books*/
 function newBook() {
-    const book = Object.create(Book());
+    const book = new Book(newTitle.value, newAuthor.value, pageCount.value, read.checked ? 'read' : 'not read');
     myLibrary.push(book);
+    book.findIndex();
+    displayMyLibrary(book);
 }
 
-function displayMyLibrary() {
-    myLibrary.forEach(book => {
-        let cover = document.createElement('div');
-        cover.setAttribute('class', 'book');
-        let title = document.createElement('h3');
-        title.setAttribute('class', 'bookTitle');
-        title.textContent = this.title;
-        cover.appendChild(title);
-        let author = document.createElement('h3');
-        author.setAttribute('class', 'author');
-        author.textContent = this.author;
-        cover.appendChild(author);
-        let pageCount = document.createElement('p');
-        pageCount.setAttribute('class', 'pages')
-        pageCount.textContent = this.pageCount;
-        cover.appendChild(pageCount);
-        let unread = document.createElement('p');
-        unread.setAttribute('class', 'unread');
-        unread.textContent = `You have ${this.read} this title.`;
-        cover.appendChild(unread);
-        let read =  document.createElement('button');
-        read.setAttribute('class', 'read')
-        read.textContent = 'Read';
-        cover.appendChild(read);
-        let remove = document.createElement('button');
-        remove.setAttribute('class', 'remove');
-        remove.textContent = 'Remove form library';
-        cover.appendChild(remove);
-    });
+function displayMyLibrary(book) {
+    let cover = document.createElement('div');
+    cover.setAttribute('class', 'book');
+    cover.setAttribute('data-index', `${myLibrary.indexOf(book)}`)
+    let title = document.createElement('h3');
+    title.setAttribute('class', 'bookTitle');
+    title.textContent = book.title;
+    cover.appendChild(title);
+    let author = document.createElement('h3');
+    author.setAttribute('class', 'author');
+    author.textContent = book.author;
+    cover.appendChild(author);
+    let pageCount = document.createElement('p');
+    pageCount.setAttribute('class', 'pages')
+    pageCount.textContent = book.pageCount;
+    cover.appendChild(pageCount);
+    let unread = document.createElement('p');
+    unread.setAttribute('class', 'unread');
+    unread.textContent = `You have ${book.read} this title.`;
+    cover.appendChild(unread);
+    let read =  document.createElement('button');
+    read.setAttribute('class', 'read')
+    read.textContent = 'Read';
+    cover.appendChild(read);
+    let remove = document.createElement('button');
+    remove.setAttribute('class', 'remove');
+    remove.textContent = 'Remove form library';
+    cover.appendChild(remove);
+
+    library.appendChild(cover);
 }
 
-function removeBook() {
-    let book = e.target.parentNode;
+function removeBook(book) {
+    myLibrary.splice(book.dataset.index, 1);
     library.removeChild(book);
+    let allBooks = [...library.childNodes];
+    for(let i = 1; i < allBooks.length; i++){
+        allBooks[i].dataset.index = i - 1;
+    }
+}
 
+function toggleRead(book, read) {
+    if(myLibrary[book].read === 'not read') {
+        myLibrary[book].read = 'read'
+    } else {
+        myLibrary[book].read = 'not read'
+    }
+
+    read.textContent = `You have ${myLibrary[book].read} this title.`;
 }
 
 /*Event listeners*/
-addAddition.addEventListener('click', (e) => {
+addBook.addEventListener('click', (e) => {
     newBook(e);
-    displayMyLibrary();
-})
+});
+library.addEventListener('click', (e) => {
+    if(e.target.classList.contains('remove')){
+        removeBook(e.target.parentNode);
+    } else if(e.target.classList.contains('read')){
+        toggleRead(e.target.parentNode.dataset.index, e.target.previousElementSibling);
+    } else return
+});
